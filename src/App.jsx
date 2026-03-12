d1a9c25c390a0a74c2ad58e34cbb4d098ad95e34
Raphael <raphael.baptiste@epitech.eu>
2026-03-12 11:05:40 +0400

    Base good

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+import Plant from "./plant.jsx";
+
 function App() {
-  return <h1 className="text-3xl font-bold text-center text-gray-800">LeBoucan</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<h1 className="text-3xl font-bold text-center text-gray-800">LeBoucan</h1>} />
+      <Route path="/plant" element={<Plant />} />
+    </Routes>
+  );
 }
 
 export default App;
