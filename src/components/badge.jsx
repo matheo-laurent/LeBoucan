7b608c5ebc4aaa11a104d1ffd45fd27e1f4847f5
gregory adenor <gregoryadenor@epitech.eu>
2026-03-12 15:36:51 +0400

    feat: ajout du système de badges avec modale de description

---
+++ src/components/badge.jsx
@@ -0,0 +1,38 @@
+import data from "../data/badge.json"
+import { BadgeCard } from "./badge-card.jsx"
+import { BadgeDesc } from "./badge-description.jsx"
+import { useState } from "react"
+
+
+export function Badge() {
+
+    const badgesData = data.badges
+    const [selectedBadge, setSelectedBadge] = useState(null)
+
+    return (
+        <div className="flex justify-center items-center min-h-screen bg-gray-100">
+          <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-3xl">
+            <h1 className="text-xl font-bold mb-4">Mes Badges</h1>
+            <div className="grid grid-cols-5 gap-4">
+              {badgesData.map(badge => (
+                <BadgeCard 
+                    key={badge.id} 
+                    name={badge.name} 
+                    img={badge.image} 
+                    inactive={!badge.earned}
+                    onClick={() => setSelectedBadge(badge)}/>
+              ))}
+            </div>
+          </div>
+          {selectedBadge &&(
+            <BadgeDesc
+                isOpen={!!selectedBadge}
+                onClose={() => setSelectedBadge(null)}
+                name={selectedBadge.name}
+                description={selectedBadge.activity}
+            />
+          )}
+
+        </div>
+    )
+}
